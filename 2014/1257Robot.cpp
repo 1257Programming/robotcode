660ad670ddbeda1258f6ff5c0db2287ca1a545d1
Brian Kilcullen <bkilcullen@ucvts.tec.nj.us>
2014-01-31 18:50:15 -0500

    Put acceleration into a separate function.

--- 2014/1257Robot.cpp
+++ 2014/1257Robot.cpp
@@ -4,7 +4,7 @@ void Team1257Robot::Autonomous()
 {
 	while(IsAutonomous() && IsEnabled())
 	{
-		Drive.SetLeftRightMotorOutputs(-.3, -.3);
+		Drive.SetLeftRightMotorOutputs(.3, .3);
 	}
 }
 
@@ -13,14 +13,7 @@ void Team1257Robot::OperatorControl()
 	while(IsOperatorControl() && IsEnabled())
 	{
 		drive(); // XBox Controller 1 drives robot
-		//XBox Controller 2 conrols arms
-		if(Stick2.GetRawAxis(1) > .1 || Stick2.GetRawAxis(1) < -.1)
-				LeftArm.Set(.2 * Stick2.GetRawAxis(1));
-		if(Stick2.GetRawAxis(4) > .1 || Stick2.GetRawAxis(4) < -.1)
-						LeftArm.Set(.2 * Stick2.GetRawAxis(1));
-		double shoulderspeed = (Stick2.GetRawAxis(2) + Stick2.GetRawAxis(5)) / 2;
-		if(shoulderspeed > .1 || shoulderspeed < -.1)
-			ArmShoulder.Set(.2 * shoulderspeed);
+		arms();//XBox Controller 2 conrols arms
 	}
 }
 
@@ -31,43 +24,38 @@ void Team1257Robot::Test()
 
 void Team1257Robot::drive()
 {
-	double reading1;
-	double reading2;
-	
-	if(Stick1.GetRawAxis(3))
+	if(Stick1.GetRawButton(5) && Stick1.GetRawButton(6))
 	{
-		reading1 = -Stick1.GetRawAxis(5); //raw speed
-		reading2 = Stick1.GetRawAxis(1); // raw curve
-		
-		if(reading1 > speed1)
-			speed1 += .05;
-		if(reading1 < .1 && reading1 > -.1)
-			speed1 = 0;
-		if(reading2 > speed2)
-			speed2 += .05;
-		if(reading2 < .1 && reading2 > -.1)
-			speed2 = 0;
-		
-		Drive.ArcadeDrive(-.5 * speed1, .5 * speed2, false);
+		Drive.TankDrive(accel(Stick1, 2, leftspeed, .5), accel(Stick1, 5, rightspeed, .5), false);
+	}
+	else if (Stick1.GetRawAxis(3)) // Back buttons
+	{
+		Drive.ArcadeDrive(accel(Stick1, 5, speed, .5), accel(Stick1, 1, curve, .5), false);
+	}
+}
 
-	}
-	else if(Stick1.GetRawButton(5) && Stick1.GetRawButton(6))
-	{
-		reading1 = -Stick1.GetRawAxis(2);
-		reading2 = -Stick1.GetRawAxis(5);
-		
-		if(reading1 > speed1)
-			speed1 += .05;
-		if(reading1 < .1 && reading1 > -.1)
-			speed1 = 0;
-		if(reading2 > speed2)
-			speed2 += .05;
-		if(reading2 < .1 && reading2 > -.1)
-			speed2 = 0;
-		Drive.SetLeftRightMotorOutputs(-.5 * speed1, -.5 * speed2);
-	}
+void Team1257Robot::arms()
+{
+	LeftArm.Set(accel(Stick2, 1, leftarmspeed, .2));
+	RightArm.Set(accel(Stick2, 4, rightarmspeed, .2));
+	if(Stick2.GetRawAxis(3) > 0)
+		ArmShoulder.Set(accel(Stick2, 2, shoulderspeed, .2));
 	else
-		Drive.SetLeftRightMotorOutputs(0, 0);
+		ArmShoulder.Set(accel(Stick2, 5, shoulderspeed, .2));
+}
+
+double Team1257Robot::accel(Joystick& stick, int axis, double& current, double limit)
+{
+	double raw = stick.GetRawAxis(axis);
+	if(raw > current && raw > 0) // If speeding up, increment it there, instead of sudden jerk
+		current += .05;
+	else if(raw < current && raw < 0) // Same as above, taking into account negative joystick values
+		current -= .05;
+	else if(dabs(raw) < dabs(current)) // If the target speed is lesser, reduce to that instantly, like for stopping
+		current = raw;
+	else if(dabs(raw) < .1) // Taking into account SLIGHTLY off-centered axes
+		current = 0;
+	return (current * limit);
 }
 
 START_ROBOT_CLASS(Team1257Robot);
--- 2014/1257Robot.h
+++ 2014/1257Robot.h
@@ -12,21 +12,38 @@ public:
 	Victor RightArm;
 	Victor ArmShoulder;
 	Gyro Angle;
-	//Ultrasonic Usonic;
+	//AnalogChannel Usonic;
 	DriverStationLCD* Lcd;
-	double speed1;
-	double speed2;
+	double leftspeed;
+	double rightspeed;
+	double speed;
+	double curve;
+	double leftarmspeed;
+	double rightarmspeed;
+	double shoulderspeed;
 	
 	Team1257Robot(): Stick1(1), Stick2(2), LeftDrive(1), RightDrive(2), Drive(LeftDrive, RightDrive),
 			LeftArm(3), RightArm(4), ArmShoulder(5), Angle(1)
 	{
 		Drive.SetExpiration(.1);
 		Lcd = DriverStationLCD::GetInstance();
-		speed1 = 0;
-		speed2 = 0;
+		leftspeed = 0;
+		rightspeed = 0;
+		leftarmspeed = 0;
+		rightarmspeed = 0;
+		shoulderspeed = 0;
 	}
 	void OperatorControl();
 	void Autonomous();
 	void Test();
+	double accel(Joystick& stick, int axis, double& current, double limit);
 	void drive();
+	void arms();
+	inline double dabs(double value)
+	{
+		if(value >= 0)
+			return value;
+		else
+			return -value;
+	}
 };
