17e7c1553ac879288662cd00c63933abb6c695a6
Team1257Kilcullen <bkilcullen@ucvts.tec.nj.us>
2014-02-17 20:36:01 -0500

    Updated code for real robot's changes to PWM ports

--- 2014/1257Robot.cpp
+++ 2014/1257Robot.cpp
@@ -11,24 +11,10 @@ void Team1257Robot::Autonomous()
 void Team1257Robot::OperatorControl()
 {
 	Lcd->Clear();
-	Lcd->Printf(DriverStationLCD::kUser_Line1, 1, "Using real robot.");
+	Lcd->Printf(DriverStationLCD::kUser_Line1, 1, "Teleoperated!");
 	Lcd->UpdateLCD();
 	while(IsOperatorControl() && IsEnabled())
 	{
-		if(Stick1.GetRawButton(7) && !realbot)
-		{
-			realbot = true; //Realbot
-			Lcd->Clear();
-			Lcd->Printf(DriverStationLCD::kUser_Line1, 1, "Using real robot.");
-			Lcd->UpdateLCD();
-		}
-		if(Stick1.GetRawButton(8) && realbot)
-		{
-			realbot = false; //Testbot
-			Lcd->Clear();
-			Lcd->Printf(DriverStationLCD::kUser_Line1, 1, "Using TestBot.");
-			Lcd->UpdateLCD();
-		}
 		drive(); // XBox Controller 1 drives robot
 		arms();//XBox Controller 2 conrols arms
 	}
@@ -42,8 +28,6 @@ void Team1257Robot::Test()
 void Team1257Robot::drive()
 {
 	double sf = .5;
-	if(!realbot)
-		sf *= -1;
 	if(Stick1.GetRawButton(5) && Stick1.GetRawButton(6))
 	{
 		Drive.TankDrive(-accel(Stick1, 2, leftspeed, sf), -accel(Stick1, 5, rightspeed, sf), false);
@@ -53,6 +37,8 @@ void Team1257Robot::drive()
 		Drive.ArcadeDrive(-accel(Stick1, 5, speed, sf), -accel(Stick1, 1, curve, sf), false);
 	}
 }
+	
+
 
 void Team1257Robot::arms()
 {
@@ -62,9 +48,8 @@ void Team1257Robot::arms()
 	LeftArm.Set(-accel(Stick2, 1, leftarmspeed, sf));
 	RightArm.Set(accel(Stick2, 4, rightarmspeed, sf));
 	DigitalInput LimitSwitch(1);
-	if(!LimitSwitch.Get())
-		ArmShoulder.Set(0);
-	else ArmShoulder.Set(accel(Stick2, 3, shoulderspeed, .4));
+	ArmShoulder1.Set(accel(Stick2, 3, shoulderspeed, .4));
+	ArmShoulder2.Set(-accel(Stick2, 3, shoulderspeed, .4));
 }
 
 double Team1257Robot::accel(Joystick& stick, int axis, double& current, double sf)
@@ -76,10 +61,11 @@ double Team1257Robot::accel(Joystick& stick, int axis, double& current, double s
 		current -= .05;
 	if(dabs(raw) < dabs(current)) // If the target speed is lesser, reduce to that instantly, like for stopping
 		current = raw;
-	if(dabs(raw) < .1) // Taking into account SLIGHTLY off-centered axes
+	if(dabs(raw) < .2) // Taking into account SLIGHTLY off-centered axes
 		current = 0;
 	return (current * sf);
 }
 
 START_ROBOT_CLASS(Team1257Robot);
+
 
