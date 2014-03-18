43c39be346bfcdf4eeefc3a16d5163d4f7820a4b
Team1257Kilcullen <bkilcullen@ucvts.tec.nj.us>
2014-03-17 20:58:30 -0400

    Update 1257Robot.h

--- 2014/1257Robot.h
+++ 2014/1257Robot.h
@@ -12,11 +12,12 @@ public:
 	Victor RightDrive1;
 	Victor RightDrive2;
 	RobotDrive Drive;
-	Victor LeftArm;
-	Victor RightArm;
+	//THEY Victor LeftArm;
+	//BWOKE Victor RightArm;
 	Victor ArmShoulder1;
 	Victor ArmShoulder2;
 	DriverStationLCD* Lcd; //Message Box
+	DigitalInput LimitSwitch;
 	double leftspeed;
 	double rightspeed;
 	double speed;
@@ -25,9 +26,9 @@ public:
 	double rightarmspeed;
 	double shoulderspeed;
 	
-	Team1257Robot(): Stick1(1), Stick2(2), LeftDrive1(1), LeftDrive2(2), RightDrive1(7), RightDrive2(8), 
+	Team1257Robot(): Stick1(1), Stick2(2), LeftDrive1(3), LeftDrive2(4), RightDrive1(9), RightDrive2(10), 
 			Drive(LeftDrive1, LeftDrive2, RightDrive1, RightDrive2), 
-			LeftArm(3), RightArm(4), ArmShoulder1(5), ArmShoulder2(6)
+			ArmShoulder1(7), ArmShoulder2(8), LimitSwitch(2)
 	{
 		Drive.SetExpiration(.1);
 		Lcd = DriverStationLCD::GetInstance();
@@ -41,7 +42,8 @@ public:
 	}
 	void OperatorControl();
 	void Autonomous();
-	void Test();	double accel(Joystick& stick, int axis, double& current, double sf);
+	void Test();	
+	double accel(Joystick& stick, int axis, double& current, double sf);
 	void drive();
 	void arms();
 	double dabs(double value)
