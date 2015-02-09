ec2416b5f7795ae1ad0df771ff39ae50b265325e
ARichman <arichman@ucvts.tec.nj.us>
2015-02-09 16:15:22 -0500

    NO ERRORS!! YAY

--- 2015/Pixy.h
+++ 2015/Pixy.h
@@ -7,10 +7,8 @@
 
 #ifndef AUTONOMOUS_PIXY_H_
 #define AUTONOMOUS_PIXY_H_
-#include "TPixy.h"
 #include <SPI.h>
-
-
+#include <cstring>
 #define PIXY_SYNC_BYTE              0x5a
 #define PIXY_SYNC_BYTE_DATA         0x5b
 #define PIXY_OUTBUF_SIZE            6
@@ -18,15 +16,56 @@
 class LinkSPI
 {
   public:
-    void init();
+    void init()
+    {
+       outLen = 0;
+    }
 
-    uint16_t getWord();
+    LinkSPI():spi(SPI::kMXP){
+    	init();
+    }
+    uint8_t getByte()
+    {
+    	uint8_t * ret = new uint8_t;
+    	spi.Read(false, ret, (uint8_t)1);
+    	return *ret;
+    }
+    uint16_t getWord()
+    {
+    	// ordering is different because Pixy is sending 16 bits through SPI
+    	      // instead of 2 bytes in a 16-bit word as with I2C
+    	      uint16_t w;
+    	      uint8_t c, cout = 0;
 
-    uint8_t getByte();
+    	      if (outLen)
+    	      {
+    	        w = transfer((uint16_t)PIXY_SYNC_BYTE_DATA);
+    	        cout = outBuf[outIndex++];
+    	        if (outIndex==outLen)
+    	          outLen = 0;
+    	      }
+    	      else
+    	        w = transfer((uint16_t)PIXY_SYNC_BYTE);
+    	      w <<= 8;
+    	      c = transfer(cout);
+    	      w |= c;
 
-    int8_t send(uint8_t *data, uint8_t len);
+    	      return w;
+    }
+    int8_t send(uint8_t *data, uint8_t len)
+    {
+          if (len>PIXY_OUTBUF_SIZE || outLen!=0)
+            return -1;
+          memcpy(outBuf, data, len);
+          outLen = len;
+          outIndex = 0;
+          return len;
+    }
 
-    void setAddress(uint8_t addr);
+    void setAddress(uint8_t addr)
+    {
+    	addr_ = addr;
+    }
 
   private:
     uint8_t outBuf[PIXY_OUTBUF_SIZE];
@@ -34,9 +73,23 @@ class LinkSPI
     uint8_t outIndex;
     uint8_t addr_;
     SPI spi;
-    uint16_t transfer(uint16_t);
-    uint8_t transfer(uint8_t);
+    uint16_t transfer(uint16_t data)
+    {
+    	union{uint16_t word; uint8_t * bytes;} input, recieved;
+    	input.word = data;
+    	spi.Write(input.bytes, sizeof(uint16_t));
+    	spi.Read(false, recieved.bytes, sizeof(uint16_t));
+    	return recieved.word;
+
+    }
+    uint8_t transfer(uint8_t data)
+    {
+    	spi.Write(&data, 1);
+    	uint8_t * ret = new uint8_t;
+    	spi.Read(false, ret, 1);
+    	return *ret;
+    }
 };
 
-
-typedef TPixy<LinkSPI> Pixy;
+#define DECL_PIXY_TYPE(cls) typedef TPixy<LinkSPI, cls> Pixy
+#endif
--- 2015/TPixy.h
+++ 2015/TPixy.h
@@ -12,7 +12,9 @@
 #define PIXY_START_WORD             0xaa55
 #define PIXY_START_WORDX            0x55aa
 #define PIXY_DEFAULT_ADDR           0x54  // I2C
-#include <RobotBase.h>
+#include <Timer.h>
+#include <string.h>
+#include <cstdlib>
 struct Block
 {
   uint16_t signature;
@@ -21,25 +23,146 @@ struct Block
   uint16_t width;
   uint16_t height;
 };
-typedef void (RobotBase::*BlockProcessCallbackPtr)(Block*);
-#define GET_CALL_PTR(clss, func) static_cast<BlockProcessCallbackPtr>(&clss::func)
-void defaultBlockProcess(Block*);
-template <class LinkType> class TPixy {
+
+
+void defaultBlockProcess(Block * blocks)
+{
+	//Do nothing
+}
+template <class LinkType, class RobotType> class TPixy {
 public:
-  TPixy(uint8_t addr=PIXY_DEFAULT_ADDR, void* procCall(Block*) = defaultBlockProcess);
-  TPixy(BlockProcessCallbackPtr, RobotBase, uint8_t addr=PIXY_DEFAULT_ADDR);
-  ~TPixy();
+	typedef void (RobotType::*BlockProcessCallbackPtr)(Block*);
+  TPixy(uint8_t addr=PIXY_DEFAULT_ADDR, void* procCall(Block*) = defaultBlockProcess)
+	{
+	  skipStart = false;
+	  blockCount = 0;
+	  blockArraySize = PIXY_INITIAL_ARRAYSIZE;
+	  blocks = (Block *)malloc(sizeof(Block)*blockArraySize);
+	  link.setAddress(addr);
+	  processBlocks = procCall;
+	  m_process = 0;
+	  m_bot = 0;
+	}
+  TPixy(void (RobotType::*ptr)(Block*), RobotType* bot, uint8_t addr=PIXY_DEFAULT_ADDR)
+  {
+    skipStart = false;
+    blockCount = 0;
+    blockArraySize = PIXY_INITIAL_ARRAYSIZE;
+    blocks = (Block *)malloc(sizeof(Block)*blockArraySize);
+    link.setAddress(addr);
+    m_classbased_proc = true;
+    m_process = ptr;
+    m_bot = bot;
+  }
+  ~TPixy()
+  {
+    free(blocks);
+  }
+
   void processBlocks(Block *);
-  uint16_t getBlocks(uint16_t maxBlocks=1000);
-  void init();
-  void update();
+  uint16_t getBlocks(uint16_t maxBlocks=1000)
+  {
+    uint8_t i;
+    uint16_t w, checksum, sum;
+    Block *block;
+
+    if (!skipStart)
+    {
+      if (getStart()==false)
+        return 0;
+    }
+    else
+  	skipStart = false;
+
+    for(blockCount=0; blockCount<maxBlocks && blockCount<PIXY_MAXIMUM_ARRAYSIZE;)
+    {
+      checksum = link.getWord();
+      if (checksum==PIXY_START_WORD) // we've reached the beginning of the next frame
+      {
+        skipStart = true;
+  	  //Serial.println("skip");
+        return blockCount;
+      }
+      else if (checksum==0)
+        return blockCount;
+
+  	if (blockCount>blockArraySize)
+  		resize();
+
+  	block = blocks + blockCount;
+
+      for (i=0, sum=0; i<sizeof(Block)/sizeof(uint16_t); i++)
+      {
+        w = link.getWord();
+        sum += w;
+        *((uint16_t *)block + i) = w;
+      }
+
+      if (checksum==sum)
+        blockCount++;
+      else
+      {
+      	//handle error
+      }
+
+  	w = link.getWord();
+      if (w!=PIXY_START_WORD)
+        return blockCount;
+    }
+  }
+  void init()
+  {
+      link.init();
+    }
+  void update()
+  {
+  	if(getBlocks())
+  	{
+  		if(!m_classbased_proc)
+  			{
+
+  			}
+  		else
+  			(m_bot->*m_process)(blocks);
+  	}
+  }
   Block *blocks;
 
 private:
-  bool getStart();
-  void resize();
+  bool getStart()
+  {
+    uint16_t w, lastw;
+
+    lastw = 0xffff;
+
+    while(true)
+    {
+      w = link.getWord();
+      if (w==0 && lastw==0)
+  	{
+        Wait(.00000010);
+  	  return false;
+  	}
+      else if (w==PIXY_START_WORD && lastw==PIXY_START_WORD)
+        return true;
+  	else if (w==PIXY_START_WORDX)
+  	{
+  	  link.getByte(); // resync
+  	}
+  	lastw = w;
+    }
+  }
+  void resize()
+  {
+    Block *newBlocks;
+    blockArraySize += PIXY_INITIAL_ARRAYSIZE;
+    newBlocks = (Block *)malloc(sizeof(Block)*blockArraySize);
+    memcpy(newBlocks, blocks, sizeof(Block)*blockCount);
+    free(blocks);
+    blocks = newBlocks;
+  }
   bool m_classbased_proc = false;
-  RobotBase m_bot;
+  RobotType *m_bot;
   BlockProcessCallbackPtr m_process;
   LinkType link;
   bool  skipStart;
