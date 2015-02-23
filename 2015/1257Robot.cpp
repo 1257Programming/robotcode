5bbde019a3b13da1d605efb9641545955c2e6f42
mallyar <rmallyas@hotmail.com>
2015-02-23 15:57:22 -0500

    Comments

--- 2015/1257Robot.cpp
+++ 2015/1257Robot.cpp
@@ -30,67 +30,103 @@ void Team1257Robot::TeleopInit()
 	CameraServer::GetInstance()->StartAutomaticCapture("cam1");
 }
 
-void Team1257Robot::TeleopPeriodic()
+void Team1257Robot::TeleopPeriodic() //NOTE: THE MOTORS TURN ARE FLIPPED, SO TO HAVE THEM GO IN THE SAME DIRECTION, ONE OF THEM MUST BE NEGATED
 {
 	double sf = .8;//scale factor for scaling drive values
 	double strafesf = 1; //scale factor for the strafe wheel since it is weaker
+
+	//1ST CONTROLER
 
 	if (Stick1.GetRawButton(5) && !Stick1.GetRawButton(6))
 		/*Safety switches - these need to be clicked to do anything
 		 * just one bumper, not both
 		 * which bumper is clicked determines which analog is move forward/back/left/right
+		 * Here - if the left bumper is clicked, the left analog is is move forward/back/left/right
+		 * the right analog is pivot
 		 */
 	{
-		Center.Set(-(float)accel(Stick1, 0, strafe, strafesf, 0.2));
+		Center.Set(-(float)accel(Stick1, 0, strafe, strafesf, 0.2)); //smoothly set the speed of the strafe to the left/right values of the left analog
 		if(!Stick1.GetRawAxis(4) || dAbs(Stick1.GetRawAxis(4)) < .2)
+			/*
+			 * account for imperfections in the analog and prevent extremely small pushes of the analog from affecting the robot
+			 * make sure that the pivot analog isn't being pushed - if so, give priority to the turning analog
+			 */
 		{
-			Left.Set(-(float)accel(Stick1, 1, forwardback, sf));
-			Right.Set((float)accel(Stick1, 1, forwardback, sf));
+			Left.Set(-(float)accel(Stick1, 1, forwardback, sf)); //smoothly set the value of the left motor
+				//to the negative of left analog forward/back (Look at the note above for why)
+			Right.Set((float)accel(Stick1, 1, forwardback, sf)); //smoothly set the value of the right motor to
+				//the left analog forward/back
 		}
-		else
+		else //turn
 		{
-			Left.Set((float)accel(Stick1, 4, turn, sf));
-			Right.Set((float)accel(Stick1, 4, turn, sf));
+			Left.Set((float)accel(Stick1, 4, turn, sf)); //smoothly set the value of the left motor to the
+				//left analog forward/back
+			Right.Set((float)accel(Stick1, 4, turn, sf)); //smoothly set the value of the right motor to the
+				//left analog forward/back
 		}
 	}
 	else if (!Stick1.GetRawButton(5) && Stick1.GetRawButton(6))
 	{
-		Center.Set(-(float)accel(Stick1, 4, strafe, strafesf, 0.2));
+		/*Safety switches - these need to be clicked to do anything
+		 * just one bumper, not both
+		 * which bumper is clicked determines which analog is move forward/back/left/right
+		 * Here - if the right bumper is clicked, the right analog is is move forward/back/left/right
+		 * the left analog is pivot
+		 */
+		Center.Set(-(float)accel(Stick1, 4, strafe, strafesf, 0.2)); //smoothly set the speed of the strafe to the value of the right analog
 		if(!Stick1.GetRawAxis(0) || dAbs(Stick1.GetRawAxis(0)) < .2)
+			/*
+			 * account for imperfections in the analog and prevent extremely small pushes of the analog from affecting the robot
+			 * make sure that the pivot analog isn't being pushed - if so, give priority to the turning analog
+			 */
 		{
-			Left.Set(-(float)accel(Stick1, 5, forwardback, sf));
-			Right.Set((float)accel(Stick1, 5, forwardback, sf));
+			Left.Set(-(float)accel(Stick1, 5, forwardback, sf)); //smoothly set the value of the left motor
+			//to the negative of right analog forward/back (Look at the note above for why)
+			Right.Set((float)accel(Stick1, 5, forwardback, sf)); //smoothly set the value of the right motor to
+			//the right analog forward/back
 		}
-		else
+		else //turn
 		{
-			Left.Set((float)accel(Stick1, 0, turn, sf));
-			Right.Set((float)accel(Stick1, 0, turn, sf));
+			Left.Set((float)accel(Stick1, 0, turn, sf)); //smoothly set the value of the left motor to the
+			//right analog forward/back
+			Right.Set((float)accel(Stick1, 0, turn, sf)); //smoothly set the value of the right motor to the
+			//right analog forward/back
 		}
 
 	}
-	else
+	else //safety switches not clicked -> DON'T MOVE
 	{
 		Left.Set(0);
 		Right.Set(0);
 		Center.Set(0);
 	}
-	if (Stick2.GetRawButton(6))
-	{
-		dSolenoid.Set(DoubleSolenoid::kForward);
-	}
-	else if(Stick2.GetRawButton(5))
-	{
-		dSolenoid.Set(DoubleSolenoid::kReverse);
-	}
-	else
-		dSolenoid.Set(DoubleSolenoid::kOff);
 
-	double liftval = Stick2.GetRawAxis(3) - Stick2.GetRawAxis(2);
+	//2ND CONTROLER
+
+	if (Stick2.GetRawButton(6)) //Right bumper clicked
+	{
+		dSolenoid.Set(DoubleSolenoid::kForward); //open the arms
+	}
+	else if(Stick2.GetRawButton(5)) //left bumper clicked
+	{
+		dSolenoid.Set(DoubleSolenoid::kReverse); //close the arms
+	}
+	else //f neither
+		dSolenoid.Set(DoubleSolenoid::kOff); //DON'T DO ANYTHING!
+
+	double liftval = Stick2.GetRawAxis(3) - Stick2.GetRawAxis(2); //get the value of the left and right triggers together
 
 	if((!bottomlimit.Get() || liftval > 0) && (!toplimit.Get() || liftval < 0))
-		Lift.Set(liftval);
+		/*
+		 * neither the top nor bottom limit switches are clicked
+		 * if it is at the bottom limit but wants to move up, allow it to move
+		 * if it is at the top limit but wants to move down, allow it to move
+		 */
+		Lift.Set(liftval); //move the lift
 	else
-		Lift.Set(0);
+		Lift.Set(0); //DON'T MOVE
+
+	//push values to the dashboard
 
 	SmartDashboard::PutString("DB/String 0", "Channel 12:");
 	SmartDashboard::PutString("DB/String 1", ToString(Pdp.GetCurrent(12)));
