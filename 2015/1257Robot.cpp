093221183ebacfa198bcbc75b155aa77c1113c52
Brian Kilcullen <bkilcullen@ucvts.tec.nj.us>
2015-02-23 15:56:58 -0500

    Commented Autonomous and Header File

--- 2015/1257Robot.cpp
+++ 2015/1257Robot.cpp
@@ -5,41 +5,35 @@ using namespace std;
 template<typename T>
 std::string ToString(T t)
 {
-	std::stringstream ss;
-	ss << t;
-	string res = "";
-	ss >> res;
-	return res;
+	std::stringstream ss; // Declare string stream
+	ss << t; // Insert numerical value into stream
+	string res = ""; // Declare std::string to store the number string
+	ss >> res; // Extract number as std::string
+	return res; // Return the result string
 }
 
 
-Team1257Robot::Team1257Robot():
-	Left(0), Right(1), Center(2), Lift(3), Stick1(0), Stick2(1), dSolenoid(4, 5), Ultra(0),
-	bottomlimit(0), toplimit(1)
-{
-	Lw = LiveWindow::GetInstance();
-}
-
-Team1257Robot::~Team1257Robot()
+Team1257Robot::Team1257Robot(): // Initialization of objects based on connection ports
+	Left(0), Right(1), Center(2), Lift(3), // PWM
+	Stick1(0), Stick2(1), //Driver Station
+	dSolenoid(4, 5), // PCM
+	bottomlimit(0), toplimit(1), // Digital IO
+	angle(0) // Analog Input
 {
 
 }
 
 void Team1257Robot::TeleopInit()
 {
-	CameraServer::GetInstance()->StartAutomaticCapture("cam1");
+	//CameraServer::GetInstance()->StartAutomaticCapture("cam1"); // Automatically send camera images
 }
 
 void Team1257Robot::TeleopPeriodic()
 {
-	double sf = .8;//scale factor for scaling drive values
-	double strafesf = 1; //scale factor for the strafe wheel since it is weaker
+	double sf = .8;
+	double strafesf = 1;
 
-	if (Stick1.GetRawButton(5) && !Stick1.GetRawButton(6))
-		/*Safety switches - these need to be clicked to do anything
-		 * just one bumper, not both
-		 * which bumper is clicked determines which analog is move forward/back/left/right
-		 */
+	if (Stick1.GetRawButton(5) && !Stick1.GetRawButton(6)) // Back button; just one, not both
 	{
 		Center.Set(-(float)accel(Stick1, 0, strafe, strafesf, 0.2));
 		if(!Stick1.GetRawAxis(4) || dAbs(Stick1.GetRawAxis(4)) < .2)
@@ -74,11 +68,11 @@ void Team1257Robot::TeleopPeriodic()
 		Right.Set(0);
 		Center.Set(0);
 	}
-	if (Stick2.GetRawButton(6))
+	if (Stick2.GetRawButton(5))
 	{
 		dSolenoid.Set(DoubleSolenoid::kForward);
 	}
-	else if(Stick2.GetRawButton(5))
+	else if(Stick2.GetRawButton(6))
 	{
 		dSolenoid.Set(DoubleSolenoid::kReverse);
 	}
@@ -103,72 +97,115 @@ void Team1257Robot::TeleopPeriodic()
 
 	SmartDashboard::PutString("DB/String 6", "Channel 15:");
 	SmartDashboard::PutString("DB/String 7", ToString(Pdp.GetCurrent(15)));
+	// Monitor current drawn by motors by printing PDP current values through CAN
 }
+
+bool ran = false; // Variable to ensure that Autonomous only runs once
 void Team1257Robot::AutonomousInit()
 {
-
+	ran = false; // Makes sure that "ran" is set to false initially
 }
 
 void Team1257Robot::AutonomousPeriodic()
 {
-	if(!bottomlimit.Get() && !toplimit.Get())
+	while(!ran) // Only run autonomous while the "ran" variable is false
 	{
-		dSolenoid.Set(DoubleSolenoid::kForward);
-		Wait(.2);
-		Lift.Set(.9);
-		Wait(2.5);
-		Lift.Set(0);
-		Right.Set(-.5);
-		Left.Set(.5);
-		Wait(1);
-		Right.Set(0);
-		Left.Set(0);
-		Lift.Set(-.55);
-		Wait(1);
-		dSolenoid.Set(DoubleSolenoid::kReverse);
-		Wait(2);
-		Center.Set(-.5);
-		Right.Set(.3);
-		Left.Set(-.3);
-		Wait(.5);
-		Center.Set(0);
-		Left.Set(0);
-		Right.Set(0);
-		Lift.Set(-.55);
-		Wait(2);
-		dSolenoid.Set(DoubleSolenoid::kForward);
-		Wait(1);
-		Lift.Set(.35);
-		Wait(1);
-		Right.Set(.5);
-		Left.Set(.5);
-		Wait(1.3);
-		Right.Set(-.75);
-		Left.Set(.75);
-		Wait(2.5);
-		Right.Set(0);
-		Left.Set(0);
-	}
-	else
-		Lift.Set(0);
+		if(!bottomlimit.Get() && !toplimit.Get()) // Makes sure that neither switch is tripped
+		{
+			dSolenoid.Set(DoubleSolenoid::kReverse); // Close arms
+			Wait(.2); // Wait for .2 seconds
+			Lift.Set(.9); // Lift arms with containers grasped
+			Wait(1.75); // Continue for 1.75 seconds
+			Lift.Set(0);// Stop lifting; high enough
+			Right.Set(-.5); // Move forward
+			Left.Set(.5); // Move forward
+			Wait(.85); // Do so for .85 seconds
+			Right.Set(0); // STOP!
+			Left.Set(0); // STOP!
+			Lift.Set(-.55); // Set container down, then keep lowering to below tote
+			Wait(0.5); // Do so for .5 seconds
+			dSolenoid.Set(DoubleSolenoid::kForward); // Open the arms
+			Right.Set(.3); // Move backwards
+			Left.Set(-.3); // To avoid having the arms get caught on tote
+			Wait(.4); //keep going back for .4 secons
+			Right.Set(0); // Stop moving forward
+			Left.Set(0); // Stop moving forward
+			Center.Set(-.5); // Slide into place to pick up tote
+			Wait(.5); // Do so for .5 seconds
+			Center.Set(0); // STOP
+			Left.Set(0); // THE
+			Right.Set(0); // DRIVE
+			Lift.Set(-.55); // Just a reminder that the elevator should STILL be going down
+			Wait(3); // JUST 3 MORE SECONDS
+			dSolenoid.Set(DoubleSolenoid::kReverse); // Close the arms to grasp the tote
+			Lift.Set(.35); // Move the elevator up to lift the tote and container just a bit
+			Wait(1); // A second should be long enough
 
+			while(TestAngle(60)) // Make a 90 degree turn, stopping the rotation after a 60-degree reading to account more momentum
+			{
+				Right.Set(-.5);
+				Left.Set(-.5);
+			}
+			Right.Set(-.65); // Move forward
+			Left.Set(.65); // Into AUTO Zone
+			Wait(1.6); // Hold high speed for 1.6 seconds
+			Right.Set(-0.5); // Reduce speed
+			Left.Set(0.5); // to .5 output
+			Wait(.3); // hold for just .3 seconds
+			Right.Set(-0.3); // Now reduce again
+			Left.Set(0.3); // to .3 output
+			Wait(.4); // Hold for .4 seconds
+			Right.Set(-0.1); // Slow again
+			Left.Set(0.1); // To .1 speed (may not actually move robot, just to help slow to a stop)
+			Wait(.5); // Hold for .5 seconds
+			Right.Set(0); // Stop the robot
+			Left.Set(0); // In the AUTO ZONE
+			Lift.Set(0); // AUTO is finished
+		}
+	else
+		Lift.Set(0); // Make sure elevator is stopped
+	ran = true; // End loop after just one iteration
+	}
+	// After ran = true, although AutonomousPeriodic continues to run, nothing happens
 }
 
 void Team1257Robot::TestInit()
 {
+	angle.Reset();
+}
 
+bool Team1257Robot::TestAngle(int theta)
+{
+	float a = angle.GetAngle();
+	if((int)dAbs(a) < theta)
+	{
+ 		return true;
+	}
+
+	else
+	{
+		return false;
+	}
 }
 
 void Team1257Robot::TestPeriodic()
 {
-	Right.Set(.5);
-	Left.Set(.5);
+	float a = angle.GetAngle();
+	if((int)dAbs(a) < 60)
+	{
+		Right.Set(-.5);
+		Left.Set(-.5);
+	}
+
+	else
+	{
+		Right.Set(0);
+		Left.Set(0);
+	}
+
+	SmartDashboard::PutString("DB/String 1", ToString((int)a));
 }
 
-void Team1257Robot::DisabledPeriodic()
-{
-
-}
 
 double Team1257Robot::accel(Joystick& Stick, int axis, double& current, double sf, double inc)
 {
--- 2015/1257Robot.h
+++ 2015/1257Robot.h
@@ -1,37 +1,36 @@
-#include "WPILib.h"
-#include <sstream>
-#include "CameraServer.h"
+#include "WPILib.h" // Necessary header file for using WPILib functions and classes
+#include <sstream> // Stringstreams are used for conversion of numbers into string
+#include "CameraServer.h" // New class used to send camera images to the dashboard
 
-class Team1257Robot : public IterativeRobot
+class Team1257Robot : public IterativeRobot // By inheriting from IterativeRobot, our program's main class does not require loops to continue operating
 {
 public:
-	CANTalon Left;
-	CANTalon Right;
-	CANTalon Center;
-	CANTalon Lift;
-	Joystick Stick1;
-	Joystick Stick2;
-	LiveWindow* Lw;
-	PowerDistributionPanel Pdp;
-	double forwardback = 0;
-	double strafe = 0;
-	double turn = 0;
-	DoubleSolenoid dSolenoid;
-	AnalogInput Ultra;
+	CANTalon Left; // The Talon SRX controller for the Left motor that drives the forward wheels on the left side
+	CANTalon Right; // The Talon SRX  controller for the Right motor that drives the forward wheels on the right side
+	CANTalon Center; // The Talon SRX for the Center motor that drives the Center slide wheel
+	CANTalon Lift; // The Talon SRX for the Elevator's motor
+	Joystick Stick1; // Manages input from the XBox 360 controller used to control the drive train
+	Joystick Stick2; // Manages input from the XBox 360 controller used to control the elevator and arms
+	PowerDistributionPanel Pdp; // Used for displaying and managing current drawn by motors
+	double forwardback = 0; // Variable determining the current output value for drive Talons in forward/backward motion
+	double strafe = 0; // Variable determining the current output value for drive Talon of the center
+	double turn = 0; // Variable determining the current output value for drive Talons of the left and right in rotation/pivoting
+	DoubleSolenoid dSolenoid; // The Solenoid valve connected to the Pneumatics Control Module
 
-	DigitalInput bottomlimit;
-	DigitalInput toplimit;
+	DigitalInput bottomlimit; // A limit switch that is pressed as the elevator hits the bottom
+	DigitalInput toplimit; // A limit switch that is pressed as the elevator hits the bottom
 
-	Team1257Robot();
-	~Team1257Robot();
+	Gyro angle; // ADW22307 Gyroscope sensor used to retrieve rotational angles in autonomous
 
-	void AutonomousInit();
-	void AutonomousPeriodic();
-	void TeleopInit();
-	void TeleopPeriodic();
-	void TestInit();
-	void TestPeriodic();
-	void DisabledPeriodic();
-	double accel(Joystick& stick, int axis, double& current, double sf, double inc = 0.5);
-	inline double dAbs(double x);
+	Team1257Robot(); // Function Prototype of class constructor
+
+	void AutonomousInit(); // Runs at AUTO start
+	void AutonomousPeriodic(); // Runs throughout AUTO
+	void TeleopInit(); // Runs at Teleop start
+	void TeleopPeriodic(); // Runs throughout teleop
+	void TestInit(); // Runs upon entering Test Mode
+	void TestPeriodic(); // Test code
+	bool TestAngle(int angle); // Returns false until the robot has completed a turn of measure "angle"
+	double accel(Joystick& stick, int axis, double& current, double sf, double inc = 0.5); // Function for managing sudden shifts in joystick values
+	inline double dAbs(double x); // Absolute value function
 };
