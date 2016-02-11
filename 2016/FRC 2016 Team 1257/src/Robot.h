2c87f8c4b654d945fca67108850fae8123a921c8
Team1257Kilcullen <bkilcullen@ucvts.tec.nj.us>
2016-02-11 15:27:29 -0500

    Actually use this file now.

--- 2016/FRC 2016 Team 1257/src/Robot.h
+++ 2016/FRC 2016 Team 1257/src/Robot.h
@@ -1,6 +1,6 @@
 #include "WPILib.h"
 
-// X-Box Controller Button ID
+// X-Box Controller Button IDs
 #define BUTTON_A 1
 #define BUTTON_B 2
 #define BUTTON_X 3
@@ -20,11 +20,13 @@
 #define AXIS_ANALOG_RIGHT_X 4
 #define AXIS_ANALOG_RIGHT_Y 5
 
+#define PI 3.14159265358979323
+
 //Global Functions
 inline double dabs(double d) { return d > 0.0 ? d : -d; } // Absolute value of a double precision floating point number
-inline bool isReasonableAxisVal(double axisVal) { return dabs(axisVal) > 0.2; } // Ensures the axis is intentionally engaged
+inline bool isReasonable(double axisVal) { return dabs(axisVal) > 0.2; } // Ensures the axis is intentionally engaged
 
-class Robot : public IterativeRobot
+class Robot: public IterativeRobot
 {
 public:
   	//Object Definitions
@@ -36,22 +38,26 @@ public:
   	Talon intakeSpin;
   	CANTalon bottomArmHinge;
   	CANTalon topArmHinge;
-  	RobotDrive driveTrain;
+
+  	ADXRS450_Gyro gyro;
+  	Encoder encDriveLeft;
+  	Encoder encDriveRight;
+  	Encoder encBottomHinge;
+  	Encoder encTopHinge;
+
   	Joystick Driver;
   	Joystick Operator;
 
   	//Variable Definitions
   	double moveVal = 0;
-  	double turnVal = 0;
+	double turnVal = 0;
 
-  	//RobotDrive (SpeedController &frontLeftMotor, SpeedController &rearLeftMotor, SpeedController &frontRightMotor, SpeedController &rearRightMotor
-public:
-  	Robot();
-
-  	void RobotInit();
-  	void AutonomousInit();
-  	void AutonomousPeriodic();
-  	void TeleopInit();
-  	void TeleopPeriodic();
-	void TestPeriodic();
+	Robot();
+	void RobotInit();
+	void AutonomousInit();
+	void AutonomousPeriodic();
+	void TeleopInit();
+	void TeleopPeriodic();
+	void SetDriveMotors(float left, float right);
+	void ArcadeDrive(float moveValue, float rotateValue, bool squaredInputs);
 };
