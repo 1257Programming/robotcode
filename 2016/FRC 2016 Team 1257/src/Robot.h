dfe91853c335c331e7a2bdbb7be25e1027f737e2
zbreit18 <zbreit18@gmail.com>
2016-02-22 17:32:45 -0500

    Added Autonomous Functionality (Hopefully)

--- 2016/FRC 2016 Team 1257/src/Robot.h
+++ 2016/FRC 2016 Team 1257/src/Robot.h
@@ -1,6 +1,7 @@
 #include "WPILib.h"
+#include <cmath>
 
-// X-Box Controller Button ID
+// X-Box Controller Button IDs
 #define BUTTON_A 1
 #define BUTTON_B 2
 #define BUTTON_X 3
@@ -20,38 +21,95 @@
 #define AXIS_ANALOG_RIGHT_X 4
 #define AXIS_ANALOG_RIGHT_Y 5
 
+#define PI 3.14159265358979323
+#define AMTRES 2048 // 2048 PPR
+#define WHEEL_DIAMETER 7.625
+#define BOTTOM_ARM_LENGTH 16.5 // INCHES
+#define TOP_ARM_LENGTH 17.875 // INCHES
+#define BOTTOM_ARM_START_ANGLE 169 //DEG
+#define TOP_ARM_START_ANGLE 2.7 //GREES
+
+// Autonomous Values (All Units in Inches)
+#define DIST_TO_DEFENSE 40 // INCHES, ROBOT LENGTH SUBTRACTED (74 - 34)
+#define DIST_LOW_BAR 82 // INCHES, ROBOT LENGTH ADDED (48 + 34)
+//Distances to get
+#define DIST_MOAT 80
+#define DIST_RAMPARTS 80
+#define DIST_ROCK_WALL 80
+#define DIST_ROUGH_TERRAIN 80
+#define DIST_CHEVAL_DE_FRISE 80
+
 //Global Functions
 inline double dabs(double d) { return d > 0.0 ? d : -d; } // Absolute value of a double precision floating point number
-inline bool isReasonableAxisVal(double axisVal) { return dabs(axisVal) > 0.2; } // Ensures the axis is intentionally engaged
+inline bool isReasonable(double axisVal) { return dabs(axisVal) > 0.2; } // Ensures the axis is intentionally engaged
 
-class Robot : public IterativeRobot
+class Robot: public IterativeRobot 
 {
 public:
-  	//Object Definitions
+  	// Object Definitions
   	CANTalon frontLeftDrive;
   	CANTalon backLeftDrive;
   	CANTalon frontRightDrive;
-    CANTalon backRightDrive;
+  	CANTalon backRightDrive;
   	CANTalon intakePivot;
   	Talon intakeSpin;
   	CANTalon bottomArmHinge;
   	CANTalon topArmHinge;
-  	RobotDrive driveTrain;
+
+  	ADXRS450_Gyro gyro;
+  	Encoder encDriveLeft;
+  	Encoder encDriveRight;
+  	Encoder encBottomHinge;
+  	Encoder encTopHinge;
+  	DigitalInput breakBeam;
+  	BuiltInAccelerometer biAccel;
+
   	Joystick Driver;
   	Joystick Operator;
 
+  	PIDController pidGyro;
+  	PIDController pidIntake;
+
   	//Variable Definitions
+
+  	double bottomhorizangle = 0;
+  	double tophorizangle = 0;
+
   	double moveVal = 0;
-  	double turnVal = 0;
+	double turnVal = 0;
 
-  	//RobotDrive (SpeedController &frontLeftMotor, SpeedController &rearLeftMotor, SpeedController &frontRightMotor, SpeedController &rearRightMotor
-public:
-  	Robot();
+	bool breakbeamenabled = false;
 
-  	void RobotInit();
-  	void AutonomousInit();
-  	void AutonomousPeriodic();
-  	void TeleopInit();
-  	void TeleopPeriodic();
-	void TestPeriodic();
+	Robot();
+	void RobotInit();
+	void AutonomousInit();
+	void AutonomousPeriodic();
+	void TeleopInit();
+	void TeleopPeriodic();
+
+	void SetDriveMotors(float left, float right);
+	void ArcadeDrive(float moveValue, float rotateValue, bool squaredInputs);
+	bool isArmOverextended();
+	double degtorad(double deg);
+    double avg(double firstValue, double secondValue);
+  
+  	// Autonomous Code
+  	void drivetoDefense();
+  	void position1();
+    void position2();
+    void position3();
+    void position4();
+    void position5();
+    void portcullis();
+    void chevalDeFrise();
+    void moat();
+    void ramparts();
+    void drawbridge();
+    void sallyPort();
+    void rockWall();
+    void roughTerrain();
+    void lowBar();
+    void driveToDefense();
+  	void driveForward(double distance, bool isFast);
+  	void turn(double endAngle);
 };
