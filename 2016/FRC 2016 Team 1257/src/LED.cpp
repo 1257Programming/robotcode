d1d855ec8c26b046e774d5e245e9c15cd48dd4d0
Tyler Wright <tw39440@gmail.com>
2016-02-23 17:48:19 -0500

    Defined all the LED mode functions

--- 2016/FRC 2016 Team 1257/src/LED.cpp
+++ 2016/FRC 2016 Team 1257/src/LED.cpp
@@ -2,40 +2,81 @@
 
 void Robot::LEDRaveMode()
 {
+  pwmOne.SetRaw(0);
+  pwmTwo.SetRaw(0);
+  pwmThree.SetRaw(0);
+  pwmFour.SetRaw(0);
 }
 
 void Robot::LEDIntakeOutBlue()
 {
+  pwmOne.SetRaw(0);
+  pwmTwo.SetRaw(0);
+  pwmThree.SetRaw(0);
+  pwmFour.SetRaw(1);
 }
 
 void Robot::LEDIntakeOutRed()
 {
+  pwmOne.SetRaw(1);
+  pwmTwo.SetRaw(0);
+  pwmThree.SetRaw(0);
+  pwmFour.SetRaw(1);
 }
 
 void Robot::LEDIntakeInBlue()
 {
+  pwmOne.SetRaw(0);
+  pwmTwo.SetRaw(0);
+  pwmThree.SetRaw(1);
+  pwmFour.SetRaw(0);
 }
 
 void Robot::LEDIntakeInRed()
 {
+  pwmOne.SetRaw(1);
+  pwmTwo.SetRaw(0);
+  pwmThree.SetRaw(1);
+  pwmFour.SetRaw(0);
 }
 
 void Robot::LEDTeleopWithBallBlue()
 {
+  pwmOne.SetRaw(0);
+  pwmTwo.SetRaw(0);
+  pwmThree.SetRaw(1);
+  pwmFour.SetRaw(1);
 }
 
 void Robot::LEDTeleopWithBallRed()
 {
+  pwmOne.SetRaw(1);
+  pwmTwo.SetRaw(0);
+  pwmThree.SetRaw(1);
+  pwmFour.SetRaw(1);
 }
 
 void Robot::LEDTeleopIdleBlue()
 {
+  pwmOne.SetRaw(0);
+  pwmTwo.SetRaw(1);
+  pwmThree.SetRaw(0);
+  pwmFour.SetRaw(0);
 }
 
 void Robot::LEDTeleopIdleRed()
 {
+  pwmOne.SetRaw(1);
+  pwmTwo.SetRaw(1);
+  pwmThree.SetRaw(0);
+  pwmFour.SetRaw(0);
 }
 
 void Robot::IdleAutonomous()
 {
+  pwmOne.SetRaw(1);
+  pwmTwo.SetRaw(1);
+  pwmThree.SetRaw(0);
+  pwmFour.SetRaw(1);
 }
+
