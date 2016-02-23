35c558af40c2fbd4d56339f6185098ba7750fd5d
Tyler Wright <tw39440@gmail.com>
2016-02-23 17:29:11 -0500

    Create LED.cpp

---
+++ 2016/FRC 2016 Team 1257/src/LED.cpp
@@ -0,0 +1,41 @@
+#include "Robot.h"
+
+void Robot::LEDRaveMode()
+{
+}
+
+void Robot::LEDIntakeOutBlue()
+{
+}
+
+void Robot::LEDIntakeOutRed()
+{
+}
+
+void Robot::LEDIntakeInBlue()
+{
+}
+
+void Robot::LEDIntakeInRed()
+{
+}
+
+void Robot::LEDTeleopWithBallBlue()
+{
+}
+
+void Robot::LEDTeleopWithBallRed()
+{
+}
+
+void Robot::LEDTeleopIdleBlue()
+{
+}
+
+void Robot::LEDTeleopIdleRed()
+{
+}
+
+void Robot::IdleAutonomous()
+{
+}
