2443b2afa0990389caf73ae64ea5a3053d9d7c44
Team1257Kilcullen <bkilcullen@ucvts.tec.nj.us>
2016-03-04 06:57:12 -0500

    Implemented but still not working.

---
+++ 2016/FRC 2016 Team 1257/src/LED.cpp
@@ -0,0 +1,81 @@
+#include "Robot.h"
+
+void Robot::LEDRaveMode()
+{
+  pwmOne.SetRaw(0);
+  pwmTwo.SetRaw(0);
+  pwmThree.SetRaw(0);
+  pwmFour.SetRaw(0);
+}
+
+void Robot::LEDIntakeOutBlue()
+{
+  pwmOne.SetRaw(0);
+  pwmTwo.SetRaw(0);
+  pwmThree.SetRaw(0);
+  pwmFour.SetRaw(255);
+}
+
+void Robot::LEDIntakeOutRed()
+{
+  pwmOne.SetRaw(255);
+  pwmTwo.SetRaw(0);
+  pwmThree.SetRaw(0);
+  pwmFour.SetRaw(255);
+}
+
+void Robot::LEDIntakeInBlue()
+{
+  pwmOne.SetRaw(0);
+  pwmTwo.SetRaw(0);
+  pwmThree.SetRaw(255);
+  pwmFour.SetRaw(0);
+}
+
+void Robot::LEDIntakeInRed()
+{
+  pwmOne.SetRaw(255);
+  pwmTwo.SetRaw(0);
+  pwmThree.SetRaw(255);
+  pwmFour.SetRaw(0);
+}
+
+void Robot::LEDTeleopWithBallBlue()
+{
+  pwmOne.SetRaw(0);
+  pwmTwo.SetRaw(0);
+  pwmThree.SetRaw(255);
+  pwmFour.SetRaw(255);
+}
+
+void Robot::LEDTeleopWithBallRed()
+{
+  pwmOne.SetRaw(255);
+  pwmTwo.SetRaw(0);
+  pwmThree.SetRaw(255);
+  pwmFour.SetRaw(255);
+}
+
+void Robot::LEDTeleopIdleBlue()
+{
+  pwmOne.SetRaw(0);
+  pwmTwo.SetRaw(255);
+  pwmThree.SetRaw(0);
+  pwmFour.SetRaw(0);
+}
+
+void Robot::LEDTeleopIdleRed()
+{
+  pwmOne.SetRaw(255);
+  pwmTwo.SetRaw(255);
+  pwmThree.SetRaw(0);
+  pwmFour.SetRaw(0);
+}
+
+void Robot::LEDAutonomousIdle()
+{
+  pwmOne.SetRaw(255);
+  pwmTwo.SetRaw(255);
+  pwmThree.SetRaw(0);
+  pwmFour.SetRaw(255);
+}
