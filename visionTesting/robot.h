26a255c6b6e5025d719359c49b3219a05b3aad9b
amandazheng <azheng1@ucvts.org>
2017-05-11 15:44:36 -0400

    Create robot.h

---
+++ visionTesting/robot.h
@@ -0,0 +1,40 @@
+/*
+ * Robot.h
+ *
+ *  Created on: Apr 17, 2017
+ *      Author: zhama
+ */
+
+#ifndef SRC_ROBOT_H_
+#define SRC_ROBOT_H_
+
+#include "WPILib.h"
+#include "CANTalon.h"
+#include <math.h>
+
+using namespace frc;
+
+class TrainingRobit : public IterativeRobot
+{
+private:
+	CANTalon FrontLeftDrive;
+	CANTalon BackLeftDrive;
+	CANTalon FrontRightDrive;
+	CANTalon BackRightDrive;
+	RobotDrive DriveTrain;
+
+	ADXRS450_Gyro Gyro;
+
+	int numberofMotors;
+public:
+	TrainingRobit();
+	void RobotInit();
+	void AutonomousInit();
+	void AutonomousPeriodic();
+	void TeleopInit();
+	void TeleopPeriodic();
+	void DisabledInit();
+	void UpdateDashboard();
+
+};
+#endif /* SRC_ROBOT_H_ */
